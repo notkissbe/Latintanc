ad37f666918758ce13294bd364e3c64ddfc17626
bence <bence@BENCE-MAIN>
2023-05-12 12:54:24 +0200

    Add project files.

---
+++ Feladatok.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Latintanc
+{
+    internal class Feladatok
+    {
+        private List<Par> lista = new List<Par>();
+
+        public List<Par> Lista { get => lista; set => lista = value; }
+
+        public void Beolvasas()
+        {
+            StreamReader r = new StreamReader("tancrend.txt");
+
+            while (!r.EndOfStream)
+            {
+                string tanc = r.ReadLine();
+                string no = r.ReadLine();
+                string ferfi = r.ReadLine();
+                lista.Add(new Par(tanc, no, ferfi));
+            }
+            r.Close();
+        }
+
+        public string Masodik()
+		{
+            
+            return $"Elso tanc: {lista[0].Tanctipus}, Utolso tanc: {lista.Last().Tanctipus}";
+
+		}
+        public string Harmadik()
+		{
+            
+		}
+    }
+}
---
+++ Par.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latintanc
+{
+    internal class Par
+    {
+        private string tanctipus;
+        private string nonev;
+        private string ferfinev;
+
+        public string Tanctipus { get => tanctipus; set => tanctipus = value; }
+        public string Nonev { get => nonev; set => nonev = value; }
+        public string Ferfinev { get => ferfinev; set => ferfinev = value; }
+
+        public Par(string tanctipus,string nonev, string ferfinev)
+        {
+            this.tanctipus = tanctipus;
+            this.nonev = nonev;
+            this.ferfinev = ferfinev;
+        }
+
+        public override string ToString()
+        {
+            return $"{tanctipus}, {nonev}, {ferfinev}\n";
+        }
+    }
+}
